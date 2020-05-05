b6bdb15e688793c7a7151a85a967fc85a1527118
Mike Allen <mikeleonardallen@gmail.com>
2020-05-04 20:51:07 -0700

    more unit tests

--- tak_env/space.py
+++ tak_env/space.py
@@ -11,7 +11,13 @@ class ActionSpace():
 
     def sample(self):
         """ Returns a array with one sample from each discrete action space """
-        valid = self.get_valid_moves(self.env.state, self.env.available_pieces, self.env.turn, self.env.continued_action)
+        state = self.env.state
+        available = self.env.available_pieces
+        turn = self.env.turn
+        continued_action = self.env.continued_action
+        stone_types = self.env.get_available_piece_types(available, turn)
+
+        valid = get_valid_moves(state, stone_types, turn, continued_action)
         return np.random.choice(valid)
 
     def place(self, state, action, available_pieces, player):
@@ -31,107 +37,105 @@ class ActionSpace():
         state = board.put(state, place_to, pieces)
         return state
 
-    def get_valid_moves(self, state, available_pieces, turn, continued_action):
-        """ Returns all current valid actions """
-        if (continued_action):
-            return self.get_available_next_moves(state, continued_action)
-        return self.get_movements(state, turn) + self.get_placements(state, available_pieces, turn)
+def get_valid_moves(state, stone_types, turn, continued_action):
+    """ Returns all current valid actions """
+    if (continued_action):
+        return get_available_next_moves(state, continued_action)
+    return get_movements(state, turn) + get_placements(state, stone_types, turn)
 
-    def get_available_next_moves(self, state, action):
-        """ 
-        Returns valid continue moves
-        Player must continue moving in same direction
-        """
-        board_size = board.get_size(state)
-        next_from = action.get('to')
-        next_to = self.get_next_space(board_size, action.get('from'), action.get('to'))
-        next_carry_limit = action.get('carry') - 1
-        return self.get_movements_from_to(state, next_from, next_to, next_carry_limit)
+def get_available_next_moves(state, action):
+    """ 
+    Returns valid continue moves
+    Player must continue moving in same direction
+    """
+    board_size = board.get_size(state)
+    next_from = action.get('to')
+    next_to = get_next_space(board_size, action.get('from'), action.get('to'))
+    next_carry_limit = action.get('carry') - 1
+    return get_movements_from_to(state, next_from, next_to, next_carry_limit)
+
+def get_placements(state, stone_types, turn):
+    """ Returns all available piece placement actions """
+    return get_combinations({
+        'action': ['place'],
+        'terminal': [True],
+        'to': board.get_open_spaces(state),
+        'piece': stone_types
+    })
+
+def get_movements(state, turn):
+    """ Returns all available piece movement actions """
+    owned = board.get_owned_spaces(state, turn)
+    available = board.get_movement_spaces(state)
+
+    moves = []
+    board_size = board.get_size(state)
+    
+    for space_owned in owned:
+        pieces = board.get_pieces_at_space(state, space_owned)
+        carry_limit = len(pieces)
+        if carry_limit > board_size:
+            carry_limit = board_size
+        # find available places to move for current space
+        for space_available in available:
+            if board.is_adjacent(space_owned, space_available):
+                moves += get_movements_from_to(state, space_owned, space_available, carry_limit)
+
+    return moves
+
+def get_movements_from_to(state, space_from, space_to, carry_limit):
+    """ Returns and array of possible ways for moving from one space to another """
+    combinations = get_combinations({
+        'carry': [i for i in range(1, carry_limit + 1)],
+        'terminal': [True, False],
+        'from': [space_from],
+        'to': [space_to],
+        'action': ['move']
+    })
+    filterFn = lambda action: is_valid_move_action(state, space_from, space_to, action)
+    return list(filter(filterFn, combinations))
+
+def get_combinations(variants):
+    """ Returns combinations given varients dictionary """
+    import itertools as it
+    varNames = sorted(variants)
+    return [dict(zip(varNames, prod)) for prod in it.product(*(variants[varName] for varName in varNames))]
 
 
-    def get_next_space(self, board_size, space_from, space_to):
-        diff = np.array(space_to) - np.array(space_from)
-        new_to = space_to + diff
+def is_valid_move_action(state, space_from, space_to, action):
+    # if terminal move and only carrying 1 piece, then the move is not valid
+    # artifact from generating all possible combinations
+    if not action.get('terminal') and action.get('carry') == 1:
+        return False
 
-        if new_to[0] not in range(board_size) or new_to[1] not in range(board_size):
-            return None
+    pieces = board.get_pieces_at_space(state, space_from)
+    
+    # cannot move here, not valid
+    if not can_move(state, space_to, pieces[:action.get('carry')]):
+        return False
 
-        return tuple(new_to)
+    # if not terminal AND next move is invalid then combination not valid
+    board_size = board.get_size(state)
+    next_space = get_next_space(board_size, space_from, space_to)
 
-    def get_placements(self, state, available_pieces, turn):
-        """ Returns all available piece placement actions """
-        return self.get_combinations({
-            'action': ['place'],
-            'terminal': [True],
-            'to': board.get_open_spaces(state),
-            'piece': self.env.get_available_piece_types(available_pieces, turn)
-        })
+    # if the top piece can move, 
+    # then there is at least one more move
+    # top piece can be capital stone
+    can_move_next = can_move(state, next_space, pieces[:1])
+    if not action.get('terminal') and not can_move_next:
+        return False
 
-    def get_movements(self, state, turn):
-        """ Returns all available piece movement actions """
-        owned = board.get_owned_spaces(state, turn)
-        available = board.get_movement_spaces(state)
-        board_size = board.get_size(state)
+    return True
 
-        moves = []
-        for space_owned in owned:
-            pieces = board.get_pieces_at_space(state, space_owned)
-            carry_limit = len(pieces)
-            if carry_limit > board_size:
-                carry_limit = board_size
-            # find available places to move for current space
-            for space_available in available:
-                if board.is_adjacent(space_owned, space_available):
-                    moves += self.get_movements_from_to(state, space_owned, space_available, carry_limit)
+def can_move(state, space_to, pieces):
 
-        return moves
-
-    def get_movements_from_to(self, state, space_from, space_to, carry_limit):
-        """ Returns and array of possible ways for moving from one space to another """
-
-        combinations = self.get_combinations({
-            'carry': [i for i in range(1, carry_limit + 1)],
-            'terminal': [True, False],
-            'from': [space_from],
-            'to': [space_to],
-            'action': ['move']
-        })
-
-        board_size = board.get_size(state)
-        next_space = self.get_next_space(board_size, space_from, space_to)
-        keep = []
-
-        for i in combinations:
-
-            # if terminal move and only carrying 1 piece, then the move is not valid
-            if not i.get('terminal') and i.get('carry') == 1:
-                continue
-
-            pieces = board.get_pieces_at_space(state, space_from)
-            pieces = pieces[:i.get('carry')]
-            
-            # cannot move here, not valid
-            if not can_move(state, space_to, pieces):
-                continue
-
-            # if not terminal and next move is invalid then combination not valid
-            can_move_next = can_move(state, next_space, pieces[-1:])
-            if not i.get('terminal') and not can_move_next:
-                continue
-
-            keep.append(i)
-
-        return keep
-
-    def get_combinations(self, variants):
-        """ Returns combinations given varients dictionary """
-        import itertools as it
-        varNames = sorted(variants)
-        return [dict(zip(varNames, prod)) for prod in it.product(*(variants[varName] for varName in varNames))]
-
-def can_move(state, space_to, pieces = []):
-
+    # no space to,
+    # TODO is it possible??
     if not space_to:
+        return False
+
+    # no pieces to move
+    if Stone(abs(pieces[0])) == Stone.EMPTY:
         return False
 
     to_piece = board.get_pieces_at_space(state, space_to)[0]
@@ -142,5 +146,16 @@ def can_move(state, space_to, pieces = []):
         return True
 
     # capital stones can move on standing stones as well
-    result = pieces[0] == Stone.CAPITAL.value and to_piece == Stone.STANDING.value and len(pieces) == 1
+    result = pieces[0] == Stone.CAPITAL.value \
+        and to_piece == Stone.STANDING.value \
+        and len(pieces) == 1
     return result
+
+def get_next_space(board_size, space_from, space_to):
+    diff = np.array(space_to) - np.array(space_from)
+    new_to = space_to + diff
+
+    if new_to[0] not in range(board_size) or new_to[1] not in range(board_size):
+        return None
+
+    return tuple(new_to)
--- tests/tak_env/test_space.py
+++ tests/tak_env/test_space.py
@@ -35,3 +35,31 @@ def describe_can_move():
   def should_account_for_top_piece(state):
     assert space.can_move(state, (1,0), [types.Stone.FLAT.value]) == False
 
+def describe_is_valid_move_action():
+
+  @fixture
+  def state():
+    return np.array([
+      [
+        [1,0,0],
+        [2,0,0],
+        [1,0,0],
+      ],
+      [
+        [1,0,2],
+        [1,0,1],
+        [1,1,2],
+      ]
+    ])
+  
+  def should_be_valid_if_valid_single_move(state):
+    assert space.is_valid_move_action(state, (0,0), (0,1), {
+      'carry': 2,
+      'terminal': True
+    }) == True
+
+  def should_not_be_valid_if_next_move_invalid(state):
+    assert space.is_valid_move_action(state, (0,0), (0,1), {
+      'carry': 2,
+      'terminal': False
+    }) == False
